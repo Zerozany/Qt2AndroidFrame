323cf2d09a9fed106d7d4cd3d220023728eb2e9c
Zerozang <ZZY990613@outlook.com>
2025-09-10 14:50:03 +0800

    feat:添加了应用程序内获取app名称以及版本信息参数

--- cmakes/importConfig.cmake
+++ cmakes/importConfig.cmake
@@ -3,13 +3,18 @@ target_compile_features(${PROJECT_NAME}
     cxx_std_23
 )
 
+# 统一定义应用名和版本号
+set(APP_NAME "Zero Studio")
+set(VERSION_NAME "1.0.0")
+
 if(ANDROID_OPTION)
     set_target_properties(${PROJECT_NAME} PROPERTIES
         QT_ANDROID_PACKAGE_SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/android"
         QT_ANDROID_PACKAGE_NAME "org.qtproject.zzy"
         QT_ANDROID_APP_ICON "@drawable/ic_launcher"
-        QT_ANDROID_APP_NAME "Zero Studio"
+        QT_ANDROID_APP_NAME "${APP_NAME}" # 应用名称
         QT_ANDROID_VERSION_CODE 1
-        QT_ANDROID_VERSION_NAME "1.0.0"
+        QT_ANDROID_VERSION_NAME "${VERSION_NAME}" # 版本号名称
+        QT_ANDROID_APPLICATION_ARGUMENTS "--appname=${APP_NAME} --version=${VERSION_NAME}" # 启动参数
     )
 endif()
